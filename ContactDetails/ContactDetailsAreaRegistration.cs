2604758cd8b025a619733b5b5efab8770bf79d51
1c3m4n <supermanlovespink@gmail.com>
2013-06-13 22:24:32 +0200

    Added Area

---
+++ ContactDetails/ContactDetailsAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MvcAssignment.Areas.ContactDetails
+{
+    public class ContactDetailsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ContactDetails";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ContactDetails_default",
+                "ContactDetails/{controller}/{action}/{id}",
+                new { controller = "Management", action = "Index", id = UrlParameter.Optional },
+                new string[] { "MvcAssignment.Areas.ContactDetails.Controllers" }
+            );
+        }
+    }
+}
